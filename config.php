569026c0f4eeaa30919fff04d68304a9a9a0086b
Muhammad Shahzaib <muhammadshahzaib6511@gmail.com>
2026-05-18 00:00:15 +0500

    fixed railway database

--- config.php
+++ config.php
@@ -1,11 +1,25 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$pass = ''; // Default XAMPP password is empty
-$db_name = 'medical_store';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-// Create connection
-$conn = new mysqli($host, $user, $pass, $db_name);
+// Railway database environment variables
+$railway_host = getenv('MYSQLHOST');
+
+if ($railway_host) {
+    $host = getenv('MYSQLHOST');
+    $user = getenv('MYSQLUSER');
+    $pass = getenv('MYSQLPASSWORD');
+    $db_name = getenv('MYSQLDATABASE');
+    $port = getenv('MYSQLPORT');
+    $conn = new mysqli($host, $user, $pass, $db_name, $port);
+} else {
+    // Local XAMPP database credentials
+    $host = 'localhost';
+    $user = 'root';
+    $pass = ''; // Default XAMPP password is empty
+    $db_name = 'medical_store';
+    $conn = new mysqli($host, $user, $pass, $db_name);
+}
 
 // Check connection
 if ($conn->connect_error) {
